28ce52fde3bdcea11c7ab4afe60ac9ec055c5d44
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-20 22:41:43 +0530

    Text core component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,10 +1,13 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'tomato' }}>
-      <View style={{ backgroundColor: 'lightblue', height: 200, width: 200 }}></View>
-      <View style={{ backgroundColor: 'lightgreen', height: 200, width: 200 }}></View>
+    <View style={{ flex: 1, backgroundColor: 'tomato', padding: 50 }}>
+      {/* <View style={{ backgroundColor: 'lightblue', height: 200, width: 200 }}></View>
+      <View style={{ backgroundColor: 'lightgreen', height: 200, width: 200 }}></View> */}
+      <Text>
+        <Text style={{ color: 'white' }}>Hello, </Text>React Native!
+      </Text>
     </View>
   );
 };
