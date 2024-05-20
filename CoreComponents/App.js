18fb79ef7549796487355c510a420fbfc4324165
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-20 22:54:19 +0530

    Image and ImageBackground core component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,13 +1,26 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
+import myImg from './assets/adaptive-icon.png';
 
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: 'tomato', padding: 50 }}>
       {/* <View style={{ backgroundColor: 'lightblue', height: 200, width: 200 }}></View>
       <View style={{ backgroundColor: 'lightgreen', height: 200, width: 200 }}></View> */}
-      <Text>
-        <Text style={{ color: 'white' }}>Hello, </Text>React Native!
-      </Text>
+      <ImageBackground source={myImg} style={{ flex: 1 }}>
+        <Text style={{ fontSize: 50, fontWeight: 'bold', color: 'blue' }}>
+          <Text style={{ color: 'white' }}>Hello, </Text>React Native!
+        </Text>
+        {/* <Image
+          source={myImg}
+          alt="testImg"
+          style={{ width: 300, height: 300 }}
+        /> */}
+        {/* <Image
+          source={{ uri: 'https://picsum.photos/300' }}
+          alt="testImg"
+          style={{ width: 300, height: 300 }}
+        /> */}
+      </ImageBackground>
     </View>
   );
 };
