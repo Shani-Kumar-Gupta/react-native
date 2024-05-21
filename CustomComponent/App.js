94230b938e7e3694b8f906b0a03e1f611c4b78bb
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-21 14:37:39 +0530

    Created Greet custom component with props

--- CustomComponent/App.js
+++ CustomComponent/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Greet from './components/Greet';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Greet name="Shani Kumar Gupta" />
+      <Greet name="Aman Kumar" />
     </View>
   );
 }
