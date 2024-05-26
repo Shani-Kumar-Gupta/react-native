f7045b8f961090adc8da0192d51b2fe89f7c7af4
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-26 22:46:08 +0530

    list rendering using map and scrollview

--- RNList/App.js
+++ RNList/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, StatusBar, Platform } from 'react-native';
+import pokemonList from './data.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((item) => {
+          return (
+            <View style={styles.pokenMonCard} key={item.id}>
+              <Text>{item.name}</Text>
+              <Text>{item.type}</Text>
+            </View>
+          );
+        })}
+        </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
   },
+  scrollView: {
+    paddingHorizontal: 16,
+    marginTop: 16
+  },
+  pokenMonCard: {
+    backgroundColor: 'white',
+    padding: 16,
+    marginBottom: 16,
+    borderRadius: 8,
+    ...Platform.select({
+      android: {
+        elevation: 5,
+        shadowOpacity: 0.3
+      }
+    })
+  }
 });
