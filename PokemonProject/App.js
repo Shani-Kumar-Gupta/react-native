39220d4beecddc6d3cf348a945c7de89e08a95a8
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-26 16:44:05 +0530

    added multiple pokemon card with dynamic data

--- PokemonProject/App.js
+++ PokemonProject/App.js
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   Platform,
   StatusBar,
+  ScrollView,
 } from 'react-native';
 import PokemonCard from './components/PokemonCard';
 
@@ -11,16 +12,48 @@ export default function App() {
   const charmanderData = {
     name: 'Charmander',
     image: require('./assets/charmander.png'),
-    type: "Fire",
+    type: 'Fire',
     hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"]
+    moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
+    weaknesses: ['Water', 'Rock'],
+  };
+
+  const squirtleData = {
+    name: 'Squirtle',
+    image: require('./assets/squirtle.png'), // Replace with the actual image path
+    type: 'Water',
+    hp: 44,
+    moves: ['Tackle', 'Water Gun', 'Tail Whip', 'Withdraw'],
+    weaknesses: ['Electric', 'Grass'],
+  };
+
+  const bulbasaurData = {
+    name: 'Bulbasaur',
+    image: require('./assets/bulbasaur.png'), // Replace with the actual image path
+    type: 'Grass',
+    hp: 45,
+    moves: ['Tackle', 'Vine Whip', 'Growl', 'Leech Seed'],
+    weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
+  };
+
+  const pikachuData = {
+    name: 'Pikachu',
+    image: require('./assets/pikachu.png'), // Replace with the actual image path
+    type: 'Electric',
+    hp: 35,
+    moves: ['Quick Attack', 'Thunderbolt', 'Tail Whip', 'Growl'],
+    weaknesses: ['Ground'],
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
-      <PokemonCard {...charmanderData} />
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...pikachuData} />
+        <PokemonCard {...bulbasaurData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
